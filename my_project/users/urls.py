ee2f11a07e810b134162a460aeac7ee3700b2557
jeeshma-sayone <jeeshma@sayonetech.com>
2023-01-04 17:59:03 +0530

    search

--- my_project/users/urls.py
+++ my_project/users/urls.py
@@ -1,8 +1,10 @@
 # users/urls.py
 from django.urls import path
 from . import views
+from .views import UserListView
 
 urlpatterns = [
     path('', views.home, name="home"),
     path("signup/", views.SignUp.as_view(), name="signup"),
+    path('search', UserListView.as_view(), name='search')
 ]
--- my_project/users/views.py
+++ my_project/users/views.py
@@ -1,7 +1,10 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from django.views.generic import ListView
 
 
 # Create your views here.
@@ -13,3 +16,14 @@ class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+# search users
+class UserListView(ListView):
+    model = User
+    template_name = 'users/user-list.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        results = User.objects.filter(Q(username__icontains=query))
+        return results
